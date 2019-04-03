8ef050ce5fb7b1e18ac58a0a7a36401063c0c3f5
steve <jeaniedaddy@gmail.com>
2019-04-03 10:39:28 -0400

    add props

--- src/index.js
+++ src/index.js
@@ -9,25 +9,37 @@ const App = () => {
     return (
         
         <div className="ui container comments">
-        <ApprovalCard />
-            <CommentDetail 
-                author="Same" 
-                avatar={faker.image.avatar()} 
-                timeAgo="Today at 4:45PM" 
-                content="Hello" 
-            />
-            <CommentDetail 
-                author="Alex" 
-                avatar={faker.image.avatar()} 
-                timeAgo="Today at 3:45PM" 
-                content="Hello2" 
-            />
-            <CommentDetail 
-                author="Jane" 
-                avatar={faker.image.avatar()} 
-                timeAgo="Yesterday at 4:45PM" 
-                content="Hello3" 
-            />
+            <ApprovalCard>
+                
+                <div>
+                    <h2>Warning!</h2>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard >
+                <CommentDetail 
+                    author="Same" 
+                    avatar={faker.image.avatar()} 
+                    timeAgo="Today at 4:45PM" 
+                    content="Hello" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Alex" 
+                    avatar={faker.image.avatar()} 
+                    timeAgo="Today at 3:45PM" 
+                    content="Hello2" 
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jane" 
+                    avatar={faker.image.avatar()} 
+                    timeAgo="Yesterday at 4:45PM" 
+                    content="Hello3" 
+                />
+            </ApprovalCard>
         </div>
     )
 };
